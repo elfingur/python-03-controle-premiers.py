30fc4a728169a8938191b9cca14f26daab5410be
Léo-LONG <leo.long@edu.esiee.fr>
2025-11-06 10:15:57 +0000

    main

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+"""Module principal qui affiche les nombres premiers inférieurs à 100."""
 from math import ceil
 
 #### Fonction secondaire
 
-
 def isprime(p):
-    if (p <= 1) :
+    """Verifie si le paramètre est un boolean
+Args : 
+    p (int) : nombre a tester
+Returns : 
+    boolean : resultat du test """
+    if p <= 1:
         return False
-    for i in range(2, int(p ** 0.5) + 1) :
-        if (p % i == 0) :
+    for i in range(2, ceil(p ** 0.5) + 1):
+        if p % i == 0:
             return False
     return True
 
 #### Fonction principale
 
-
 def main():
-
+    """Fonction principale qui affiche les nombres premiers de 0 à 99."""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
